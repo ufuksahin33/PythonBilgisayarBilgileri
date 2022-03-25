17fb95dda698a075e56b28a119410d53eed5406a
Ufuk Şahin <ufuksahinzx@gmail.com>
2022-03-26 00:07:40 +0300

    Python  ve Bilgisayar Bilgisi
    Python ve Bilgisayar Özelliklerinizi öğreneceğiniz bir örnek kodlamam.

---
+++ bılgısayarbılgılerı/bılgısayarbılgılerı.py
@@ -0,0 +1,19 @@
+import platform
+
+# İşletim sistemini öğrenmek.
+print(platform.system())
+
+#İşlemci bilgisini öğrenmek.
+print(platform.processor())
+
+#Bilgisayarın Kullanıcı Adını Öğrenmek.
+print(platform.platform.node())
+
+#Python Versiyonunu Öğrenmek.
+print(platform.python_version())
+
+#Python Compiler bilgisini öğrenmek.
+print(platform.python_compiler())
+
+#Python İmplementasyonunu öğrenmek.
+print (platform.python_implementation())
